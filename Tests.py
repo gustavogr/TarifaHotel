54579ae6ae240adc85da972db98774d246f21efa
Genessis Sanchez <genny@genny.ac.labf.usb.ve>
2015-01-20 00:36:28 -0430

    Tests de fronteras

--- Tests.py
+++ Tests.py
@@ -46,6 +46,35 @@ class TestCalcularPrecio(unittest.TestCase):
         self.assertEqual(calcularPrecio(datetime.datetime(2015,1,19,5,59),
                                         datetime.datetime(2015,1,19,16,47),
                                         Tarifa(10,20)), 120)
+      # Casos limites de la libreria
+    def testMinimoLibreria(self):
+        self.assertEqual(calcularPrecio(datetime.datetime(MINYEAR,1,1,0,0),
+                                        datetime.datetime(MINYEAR,1,1,1,2),
+                                        Tarifa(50,60)), 120)
+    def testMaximoLibreria(self):
+        self.assertEqual(calcularPrecio(datetime.datetime(MAXYEAR,12,28,23,59),
+                                        datetime.datetime(MAXYEAR,12,31,23,59),
+                                        Tarifa(50,60)), 3990)
+    # Caso tarifa unitaria
+    def testTarifaDiariaUnitaria(self):
+        self.assertEqual(calcularPrecio(datetime.datetime(2015,1,19,6,10),
+                                        datetime.datetime(2015,1,19,19,18),
+                                        Tarifa(1,1)), 14)
+        
+    # Tests de frontera sobre la salida (maxima y minima)
+    def testQuinceMinutosDia(self):
+        self.assertEqual(calcularPrecio(datetime.datetime(2015,1,19,14,0),
+                                        datetime.datetime(2015,1,19,14,15),
+                                        Tarifa(50,60)), 50)
+
+    def testQuinceMinutosNoche(self):
+        self.assertEqual(calcularPrecio(datetime.datetime(2015,1,19,19,0),
+                                        datetime.datetime(2015,1,19,19,15),
+                                        Tarifa(50,60)), 60)
+    def testMaximoSetentayDosHoras(self):
+        self.assertEqual(calcularPrecio(datetime.datetime(2015,1,10,17,55),
+                                        datetime.datetime(2015,1,13,17,55),
+                                        Tarifa(50,60)), 3990)
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
