47021088d04db15040d7174ecde95fbeaf24450b
Genessis Sanchez <genny@genny.ac.labf.usb.ve>
2015-01-19 23:35:28 -0430

    test1

---
+++ Tests.py
@@ -0,0 +1,23 @@
+'''
+Created on 19/01/2015
+
+@authors:     Genessis Sanchez
+              Gustavo Gutierrez
+'''
+import unittest
+from Reserva import *
+from datetime import MINYEAR
+from datetime import MAXYEAR
+
+class TestCalcularPrecio(unittest.TestCase):
+        
+    # Tests de frontera sobre la entrada  
+    # Casos unica tarifa
+    def testLimiteInferiorDiariaConNocturna(self):
+        self.assertEqual(calcularPrecio(datetime.datetime(2015,1,19,6,1),
+                                        datetime.datetime(2015,1,19,8,15),
+                                        Tarifa(50,60)), 150)
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
