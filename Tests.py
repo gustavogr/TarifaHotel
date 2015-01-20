5e655c07763f55306d3a7c5f0ae5581dd244e068
Genessis Sanchez <genny@genny.ac.labf.usb.ve>
2015-01-19 23:54:48 -0430

    Casos de frontera con tarifa unica

--- Tests.py
+++ Tests.py
@@ -17,7 +17,18 @@ class TestCalcularPrecio(unittest.TestCase):
         self.assertEqual(calcularPrecio(datetime.datetime(2015,1,19,6,1),
                                         datetime.datetime(2015,1,19,8,15),
                                         Tarifa(50,60)), 150)
-
+    def testLimiteSuperiorDiariaConNocturna(self):
+        self.assertEqual(calcularPrecio(datetime.datetime(2015,1,19,14,23),
+                                        datetime.datetime(2015,1,19,17,59),
+                                        Tarifa(50,60)), 200)
+    def testLimiteInferiorNocturnaConDiaria(self):
+        self.assertEqual(calcularPrecio(datetime.datetime(2015,1,19,18,1),
+                                        datetime.datetime(2015,1,20,0,0),
+                                        Tarifa(50,60)), 360)
+    def testLimiteSuperiorNocturnaConDiaria(self):
+        self.assertEqual(calcularPrecio(datetime.datetime(2015,1,19,0,0),
+                                        datetime.datetime(2015,1,19,5,59),
+                                        Tarifa(50,60)), 360)
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
     unittest.main()
